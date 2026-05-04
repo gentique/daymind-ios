ee0356b358b9965d4c32936b51be000750d92780
Gentian Barileva <gentian.barileva@airalo.com>
2026-05-04 12:52:11 +0200

    add base proj

---
+++ Daymind/DaymindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaymindApp.swift
+//  Daymind
+//
+//  Created by Gentian Barileva on 4.5.26.
+//
+
+import SwiftUI
+
+@main
+struct DaymindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
